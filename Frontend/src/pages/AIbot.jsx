7821d11c4f7f4d4f636bc55a98375fb19eb3d7c4
Anurag Lowanshi <anuragrocksss007@gmail.com>
2025-07-25 13:39:32 +0530

    add data function

--- Frontend/src/pages/AIbot.jsx
+++ Frontend/src/pages/AIbot.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+ 
 const AIbot = () => {
+      const data = function(){
+            
+      }
   return (
     <div>Integrate AI Bot</div>
   )
 }
 
 export default AIbot
+
